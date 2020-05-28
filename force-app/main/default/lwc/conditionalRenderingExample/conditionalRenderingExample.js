cdec613c5e5ea9371b782f59140b03e8e012527f
Refayat Haque <refayathaque@gmail.com>
2020-05-27 20:09:24 -0400

    May 27 2009

--- force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
+++ force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
@@ -1,8 +1,12 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, track } from 'lwc';
+
+// Properties decorated with @track, creates a one way data binding between controller and template, template will auto re-render if property value changes here
 
 export default class ConditionalRenderingExample extends LightningElement {
+    // LWC bundle name must be camelCase, whereas controller class name must be in PascalCase
     @track displayDiv = false;
     showDivHandler(event) {
         this.displayDiv = event.target.checked;
     }
+    // Handler functions create the two way data binding between the template and controller
 }
